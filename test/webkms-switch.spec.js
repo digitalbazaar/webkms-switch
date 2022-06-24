d09492684cf518f189e311d00123b1bb2e221916
David I. Lehn <dlehn@digitalbazaar.com>
2022-06-24 00:03:28 -0400

    Update copyright date.

--- test/webkms-switch.spec.js
+++ test/webkms-switch.spec.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2022 Digital Bazaar, Inc. All rights reserved.
  */
 import chai from 'chai';
 chai.should();
